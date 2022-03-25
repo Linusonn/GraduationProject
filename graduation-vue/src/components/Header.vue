500edbd5c2ee01c8141de0fc88fcee44ac83a28e
Linusonn's Desktop <linusonn@hotmail.com>
2022-03-25 23:23:48 +0800

    developing

---
+++ graduation-vue/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div style="height: 50px; lint-height: 50px; border-bottom: 1px solid #ccc; display: flex">
+        <div style="width: 200px">后台管理</div>
+        
+    </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default ({
+    name: "Header"
+})
+</script>
+
+<style scoped>
+
+</style>
