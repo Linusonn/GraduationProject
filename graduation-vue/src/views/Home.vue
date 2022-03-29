54732cd0c3cde82040adb7320286133d7a746a0c
Linusonn's Desktop <linusonn@hotmail.com>
2022-03-29 22:52:52 +0800

    P1 Finish

--- graduation-vue/src/views/Home.vue
+++ graduation-vue/src/views/Home.vue
@@ -1,10 +1,43 @@
 <template>
   <div style="padding: 10px">
-    <el-table :data="tableData" stripe style="width: 100%">
-      <el-table-column prop="date" label="Date" />
-      <el-table-column prop="name" label="Name" />
-      <el-table-column prop="address" label="Address" />
+<!--    功能区域-->
+    <div style="margin: 10px 0">
+      <el-button type="primary">新增</el-button>
+      <el-button type="primary">导入</el-button>
+      <el-button type="primary">导出</el-button>
+    </div>
+<!--    搜索区域-->
+    <div style="margin: 10px 0">
+      <el-input v-model="search" placeholder="Please input" style="width: 20%"/>
+      <el-button type="primary" style="margin-left: 5px">查询</el-button>
+    </div>
+
+    <el-table :data="tableData" border stripe style="width: 100%">
+      <el-table-column prop="date" label="Date" sortable/>
+      <el-table-column prop="name" label="Name" sortable/>
+      <el-table-column prop="address" label="Address" sortable/>
+      <el-table-column fixed="right" label="Operations" width="120">
+        <template #default>
+          <el-button type="text"  @click="handleEdit">Edit</el-button>
+          <el-popconfirm title="Are you sure to delete this?">
+            <template #reference>
+              <el-button type="text" >Delete</el-button>
+            </template>
+          </el-popconfirm>
+        </template>
+      </el-table-column>
     </el-table>
+    <div style="margin: 10px 0">
+      <el-pagination
+          :currentPage="currentPage"
+          :page-size="10"
+          :page-sizes="[5, 10, 20]"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+      />
+    </div>
   </div>
 </template>
 
@@ -19,6 +52,7 @@ export default {
   },
   data() {
     return{
+      search:'',
       tableData:[
         {
           date: '2016-05-03',
@@ -40,7 +74,20 @@ export default {
           name: 'Tom',
           address: 'No. 189, Grove St, Los Angeles',
         },
-      ]
+      ],
+      total:10,
+      currentPage:1,
+    }
+  },
+  methods:{
+    handleEdit(){
+
+    },
+    handleSizeChange(){
+
+    },
+    handleCurrentChange(){
+
     }
   }
 }
